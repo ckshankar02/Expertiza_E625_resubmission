73f0b8e45260f05cdaa773123145b7f8453d6b1d
Shankar <shankar.c.k.02@gmail.com>
2012-10-31 15:05:29 -0400

    "Resubmission changes"

--- test/functional/response_controller_test.rb
+++ test/functional/response_controller_test.rb
@@ -10,6 +10,8 @@ class ResponseControllerTest < ActionController::TestCase
     fixtures :questions
     fixtures :participants
     fixtures :questionnaires
+    fixtures :team_role_assignments
+    fixtures :team_member_roles
 
     def test_team_role_info
        assert_not_nil(team_roles(:team_role1).id)
@@ -17,14 +19,15 @@ class ResponseControllerTest < ActionController::TestCase
 
     def test_response_new
        t = TeammateReviewResponseMap.find(:first, :conditions=> ["reviewee_id = ? and reviewer_id = ? and  reviewed_object_id = ?", participants(:par17).id, participants(:par18).id,assignments(:assignment_oss).id])
-       rt = ResponseMap.find_by_type("RoleBasedReviewResponseMap")
+       #t =  response_maps(:response_maps9).id
+       rt = response_maps(:response_maps10).id
        role = team_roles(:team_role1).id
        u = users(:student3).id
-       assert_not_nil(t)
-       assert_not_nil(rt)
+       #assert_equal("ques",t.type)
+       #assert_equal("make",rt.type)
        assert_not_nil(u)
-       assert_not_nil(role)
-       post :new, {:id => t.id,:return=>"teammate",:rolemap_id => rt.id,:reviewed_member => u,:role_info=> role}
+      # assert_equal("esting",role.role_names)
+       post :new, {:id => t,:return=>"teammate",:rolemap_id => rt,:reviewed_member => u,:role_info=> role}
        assert_response(:success)
     end
 
